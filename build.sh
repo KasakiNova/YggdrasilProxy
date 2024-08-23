a8f1b23882d156a172563cde40e47946a4ab30d6
织梦雨梵 <retayihtevr@qq.com>
2024-08-23 14:09:25 +0800

    更新编译脚本

--- build.sh
+++ build.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/bash
 
 # 设定变量
-outputFileName="YggdrasilProxy"
+version="snapshots"
+platform=$(uname -p)
+outputFileName="YggdrasilProxy-${platform}-${version}"
 outputPath="build"
 sourceFile="main.py"
 systemThread=$(nproc)
-pypiLink="https://pypi.mirrors.ustc.edu.cn/simple"
-# 判断输出目录是否存在
-if [ -d "${outputPath}" ];then
-    echo -e "Folder already exists \nDelete Folder"
-    rm -rf ${outputPath}
-else
-    echo -e "Folder does not exist \nCreate Folder"
-fi
+pypiLink="https://pypi.python.org/simple"
+# pypiLink="https://pypi.mirrors.ustc.edu.cn/simple/"
+
+rm -rf ./${outputPath}
 mkdir -p ${outputPath}
 
 # Python虚拟环境
-if [ -d "./venv" ];then
-    rm -rf ./venv
+if [ ! -d "./buildVenv" ];then
+  /usr/bin/python3 -m venv ./buildVenv
 fi
-/usr/bin/python3 -m venv venv
-source ./venv/bin/activate
+
+source ./buildVenv/bin/activate
+
+pip install --upgrade pip
 pip install -r requirements.txt -i ${pypiLink}
 pip install nuitka -i ${pypiLink}
 
-time python3 -m nuitka --follow-imports --standalone --onefile --show-memory  --show-progress --include-package=requests --jobs=${systemThread} --output-dir=${outputPath} --output-filename=${outputFileName} ${sourceFile}
+time python3 -m nuitka \
+  --follow-imports --standalone --onefile \
+  --show-memory --show-progress \
+  --include-package=requests --jobs="${systemThread}" \
+  --output-dir=${outputPath} --output-filename="${outputFileName}" \
+  ${sourceFile}
 
 # 退出虚拟环境
 deactivate
