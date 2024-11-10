8a890cd0e63a095d704ec4749bc5514a8292e5c7
织梦雨梵 <retayihtevr@qq.com>
2024-11-10 16:16:00 +0800

    add some notes

--- modules/utils/proxies.py
+++ modules/utils/proxies.py
@@ -5,6 +5,7 @@ class Proxies:
         local_proxies = {}
         proxy_settings = gVar.cfgContext['Proxy']
         if proxy_settings['enable']:
+            # If enable auth
             if proxy_settings['enable_auth']:
                 protocol = proxy_settings['address'].split("://")[0]
                 link = proxy_settings['address'].split("://")[1]
@@ -12,6 +13,7 @@ class Proxies:
                     "http": f"{protocol}://{proxy_settings['username']}:{proxy_settings['password']}@{link}",
                     "https": f"{protocol}://{proxy_settings['username']}:{proxy_settings['password']}@{link}",
                 }
+            # no auth
             else:
                 local_proxies = {"http": f"{proxy_settings['address']}", "https": f"{proxy_settings['address']}"}
         gVar.proxies = local_proxies
