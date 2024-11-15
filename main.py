ef2e0f28c02413caa3d91e148afb3c3a7671a033
KasakiNova <kasakinova@outlook.com>
2024-11-15 21:11:33 +0800

    Detaching WSGI threads

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import sys
+import threading
 from time import sleep
-from colorama import Fore, Style
+from print_color import print
 from paste.translogger import TransLogger
 from waitress import serve
 
@@ -12,6 +13,7 @@ from modules.services.publickeys import PublicKeys
 from modules.services.blacklistService import BlacklistService
 from modules.utils.proxies import Proxies
 from modules.utils.sysinfo import sysinfo
+from modules.console.mainConsole import MainConsole
 
 
 def initialize_config() -> None:
@@ -20,9 +22,9 @@ def initialize_config() -> None:
     sleep(0.001)
     if cfg.init():
         gVar.cfgContext = cfg.read()
-        print(Fore.GREEN + "[Config File Loaded]" + Style.RESET_ALL)
+        print("Config File Loaded", tag='Success', tag_color='green', color='white')
     else:
-        sys.exit(Fore.RED + "[Please check your config and try again]" + Style.RESET_ALL)
+        sys.exit(print("Please check your config and try again", tag='ERROR', tag_color='red', color='white'))
 
 
 def initialize_services() -> None:
@@ -33,12 +35,12 @@ def initialize_services() -> None:
     publickeys = PublicKeys()
     publickeys.start_thread()
     # init blacklist service
-    blacklist = BlacklistService('blacklist.json')
-    # blacklist.add_blacklist_by_name("wdsda")
+    # blacklist = BlacklistService('blacklist.json')
+    # blacklist.add_user_by_name("wdsda")
     # Init Proxies Link
     proxies = Proxies()
     if gVar.proxies and not proxies.check_proxies():
-        sys.exit(Fore.RED + "[Proxy is incorrect]" + Style.RESET_ALL)
+        sys.exit("Proxy is incorrect")
 
 
 def start_waitress(thread=10) -> None:
@@ -57,11 +59,9 @@ def start_waitress(thread=10) -> None:
             channel_timeout=20,
             max_request_body_size=10 * 1024 * 1024
         )
-    except KeyboardInterrupt:
-        print("\nStopped by user")
     except PermissionError as e:
         # # Maybe for Windows
-        if hasattr(e, 'winerror') and e.winerror == 10013: # This error code 10013 for windows Port is in use
+        if e.winerror == 10013: # This error code 10013 for windows Port is in use
             print(f"Error: Port {gVar.cfgContext['General']['port']} is already in use.")
         print(f"Permission Error: {e}")
     except OSError as e:
@@ -69,6 +69,26 @@ def start_waitress(thread=10) -> None:
         if e.errno == 98: # Error code 98 is port already use
             print(f"Error: Port {gVar.cfgContext['General']['port']} is already in use.")
         print(f"OS Error: {e}")
+
+
+# Don't use other WSGI server, just use flask
+def start_flask_app() -> None:
+    from modules.services.httpLogic import app
+    app.run(host=gVar.cfgContext["General"]["ip"],
+            port=gVar.cfgContext["General"]["port"],
+            debug=gVar.debugMode,
+            threaded=True
+            )
+
+
+# Start WSGI Server thread
+def run_wsgi_server() -> None:
+    if gVar.debugMode:
+        # Just start flask Server
+        start_flask_app()
+    else:
+        # not debug, run waitress
+        start_waitress()
 
 
 def main() -> None:
@@ -85,16 +105,15 @@ def main() -> None:
         print("Config: \n", gVar.cfgContext)
         if gVar.cfgContext['Proxy']['enable']:
             print("ProxiesLink: \n", gVar.proxies)
-    # Start Server
-        from modules.services.httpLogic import app
-        app.run(host=gVar.cfgContext["General"]["ip"],
-                port=gVar.cfgContext["General"]["port"],
-                debug=True,
-                threaded=True
-                )
-    else:
-        start_waitress()
 
+    # Create a new thread to run http server
+    http_thread = threading.Thread(target=run_wsgi_server)
+    http_thread.daemon=True
+    http_thread.start()
+
+    # Running Console
+    sleep(0.3)
+    MainConsole().cmdloop()
 
 if __name__ == '__main__':
     main()
